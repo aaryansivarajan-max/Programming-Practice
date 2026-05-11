a88a76fb07d6d8e03df6dc7f60eb28d2c1e8c037
Aaryan Chulliparambil Sivarajan <aaryan.sivarajan@gmail.com>
2026-05-11 11:40:33 +0530

    Added some code to get max altitude and max range ,has some errors as it gives 0 for both

--- C programs/2dsim.c
+++ C programs/2dsim.c
@@ -10,17 +10,32 @@ struct initial {
     double Xi;
     double Yi;
 };
-double calc_dx(struct  initial init,double t){
+double calc_x(struct  initial init,double t){
     double theta_rad = init.anglei * (pi/180.0);
-    double dx = init.Xi+(init.Vi *cos(theta_rad) *  t);
-    return dx;
+    double x = init.Xi+(init.Vi *cos(theta_rad) *  t);
+    return x;
 
 }
-double calc_dy(struct initial init, double t){
+double calc_y(struct initial init, double t){
     double theta_rad=init.anglei *(pi / 180.0); // converted angle into  radians since c math libs uses radians
     double Vy =init.Vi * sin(theta_rad); // since dy=vy*t-0.5*g*t*t
-    double dy =init.Yi + (Vy*t)-0.5 * g * t * t;
-    return dy;
+    double y =init.Yi + (Vy*t)-0.5 * g * t * t;
+    return y;
+}
+
+double max_height(struct initial rocket){
+    double theta_rad = rocket.anglei * (pi / 180);
+    double Vy_i = rocket.Vi * sin(theta_rad);
+    return rocket.Yi + (pow(Vy_i,2) / (2 * g));
+
+}
+double max_range(struct initial rocket){
+    double theta_rad=rocket.anglei * (pi / 180);
+    double Vx = rocket.Vi * cos(theta_rad);
+    double Vy = rocket.Vi * sin(theta_rad);
+    double discriminant = sqrt(pow(Vy,2) + (2 * g * rocket.Yi));
+    return rocket.Xi + (Vx / g) * (Vy + discriminant);
+
 }
 
 int main() {
@@ -28,10 +43,15 @@ int main() {
     printf("PROJECTILE MOTION SIMULATION\n");
     printf("TIME(SECONDS) | X POS(METRE) | Y POS(METRE)\n");
     printf("---------------------------------------------\n");
-    for (double t=0; t <= 5.0;t += 0.5){
-        double x =calc_dx(rocket,t);
-        double y=calc_dy(rocket,t);
+    for (double t=0; t <= 10.0;t += 0.5){
+        double x =calc_x(rocket,t);
+        double y=calc_y(rocket,t);
+        if (y < 0)break;
         printf("%7.1f  |  %8.2f  | %8.2f\n",t,x,y);
     }
-    return 0;
+
+    printf("\n---Ballistics Report---\n");
+    printf("Max Altitude : %.2f\n",max_height);
+    printf("Impact Range : %.2f\n",max_range);
+        return 0;
 }
